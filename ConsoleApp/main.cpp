c7415d320970414546400cef0afa1ea66395a7cc
Alexander Popov <lxndrppv@gmail.com>
2025-10-28 16:06:26 +0200

    feat: solution

--- ConsoleApp/main.cpp
+++ ConsoleApp/main.cpp
@@ -1,7 +1,37 @@
 ﻿#include <iostream>
 
+using namespace std;
+
+class Solution {
+public:
+    int totalMoney(int n) {
+        int total = 0;
+        
+        auto weeks = n / 7 + 1;
+        for (size_t i = 0; i < weeks; i++) {
+            int mondayAmount = i + 1;
+            if (i == weeks - 1) {
+                // not full week
+                auto days = n % 7;
+                for (size_t j = 0; j < days; j++) {
+                    total += mondayAmount + j;
+                }
+            }
+            else {
+                for (size_t j = 0; j < 7; j++) {
+                    total += mondayAmount + j;
+                }
+            }
+        }
+
+        return total;
+    }
+};
+
 static void test1() {
-    printf("end\n");
+    constexpr auto n = 10;
+    Solution s;
+    cout << "For " << n << " days you get " << s.totalMoney(n) << endl;
 }
 
 static void runTests() {
