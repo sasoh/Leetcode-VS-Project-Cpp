0e0e1e430f27d4a09d8897408edb5dda46d6762d
Alexander Popov <lxndrppv@gmail.com>
2026-03-29 11:30:21 +0300

    feat: complex class

--- ConsoleApp/main.cpp
+++ ConsoleApp/main.cpp
@@ -1,23 +1,28 @@
 ﻿#include <iostream>
 #include <vector>
 #include <format>
-#include "Square.h"
+#include "Complex.h"
 using namespace std;
 
 int main() {
-    vector<Square> squares{
-        Square{1}, Square{2}, Square{4}, Square{10}, Square{15}
+    vector<Complex> c{
+        Complex{}, Complex{1, 1}, Complex{2, 4}, Complex{5, 5} 
     };
 
-    for (const auto& s : squares) {
-        cout << format(
-            "Square with side = {:<4} area = {:<4} perimeter = {:<4} diagonal = {:<4.2f}\n",
-            s.getSide(),
-            s.getArea(),
-            s.getPerimeter(),
-            s.getDiagonal()
-        );
-    }
+    //for (const auto& i : c) {
+    //    cout << format("{}\n", i.toString());
+    //}
+
+    auto c1 = *(c.end() - 1);
+    auto c2 = c[1];
+
+    cout << format("{} - {} = ", c1.toString(), c2.toString());
+    c1.subtract(c2);
+    cout << format("{}\n", c1.toString());
+
+    cout << format("{} + {} = ", c1.toString(), c2.toString());
+    c1.add(c2);
+    cout << format("{}\n", c1.toString());
 
     return 0;
 }
