19e41d497f749b33ec9ad5f14b02c148736e26cf
Alexander Popov <lxndrppv@gmail.com>
2026-04-11 11:40:17 +0300

    feat: solution

--- ConsoleApp/main.cpp
+++ ConsoleApp/main.cpp
@@ -1,15 +1,29 @@
 ﻿#include <iostream>
-
+#include <vector>
+#include <unordered_map>
+#include <string>
+#include <utility>
+#include <numeric>
+#include <format>
 using namespace std;
 
-static void test1() {
-}
-
-static void runTests() {
-    test1();
-}
-
 int main() {
-    runTests();
+    auto m = unordered_map<string, vector<int>>{
+        {"Susan", {92, 85, 100}},
+        {"Eduardo", {83, 95, 79}},
+        {"Azizi", {91, 89, 82}},
+        {"Pantipa", {97, 91, 92}},
+    };
+
+    auto totalAvg{0.0};
+    for (const pair<string, vector<int>>& i : m) {
+        auto avg{ (accumulate(i.second.cbegin(), i.second.cend(), 0.0)) / i.second.size() };
+        cout << format("Average for {} is {:.2f}\n", i.first, avg);
+        totalAvg += avg;
+    }
+
+    auto classAvg{ totalAvg / m.size() };
+    cout << format("Class average is {:0.2f}\n", classAvg);
+
     return 0;
 }
