e026a70f6ba2aa502dea91778169743890f37235
Alexander Popov <lxndrppv@gmail.com>
2026-04-21 11:41:08 +0300

    feat: fixed operators; switched from size_t to ints for simplicity

--- ConsoleApp/main.cpp
+++ ConsoleApp/main.cpp
@@ -3,20 +3,22 @@
 #include <stdexcept>
 #include <utility>
 #include <algorithm>
+#include <random>
+#include <limits>
 using namespace std;
 
 // int[]-based version (so we can show off unique_ptr)
 class Matrix {
 public:
-    Matrix(size_t rows, size_t cols) : m_rows{ rows }, m_cols{ cols }, m_m{ make_unique<int[]>(rows * cols) } {}
+    Matrix(int rows, int cols) : m_rows{ rows }, m_cols{ cols }, m_m{ make_unique<double[]>(rows * cols) } {}
     ~Matrix() noexcept {}
-    Matrix(const Matrix& other) : m_rows{ other.m_rows }, m_cols{ other.m_cols }, m_m{ make_unique<int[]>(other.m_rows * other.m_cols) } {
+    Matrix(const Matrix& other) : m_rows{ other.m_rows }, m_cols{ other.m_cols }, m_m{ make_unique<double[]>(other.m_rows * other.m_cols) } {
         std::copy(other.m_m.get(), other.m_m.get() + (m_rows * m_cols), m_m.get());
     }
     Matrix& operator=(const Matrix& other) {
         m_rows = other.m_rows;
         m_cols = other.m_cols;
-        m_m = make_unique<int[]>(m_rows * m_cols);
+        m_m = make_unique<double[]>(m_rows * m_cols);
         std::copy(other.m_m.get(), other.m_m.get() + (m_rows * m_cols), m_m.get());
         return *this;
     }
@@ -29,43 +31,92 @@ public:
         }
         return *this;
     }
-    int& operator()(int row, int col) {
+    const int rows() const noexcept { return m_rows; }
+    const int cols() const noexcept { return m_cols; }
+    double& operator()(int row, int col) {
         if (row >= m_rows || col >= m_cols) {
             throw std::out_of_range("Index out of bounds");
         }
         return m_m[index(col, row)];
     }
-    friend Matrix& operator+(const Matrix& m1, const Matrix& m2) {
-        if (m1.m_rows != m2.m_rows || m1.m_cols != m2.m_cols) {
+    const double& operator()(int row, int col) const {
+        if (row >= m_rows || col >= m_cols) {
+            throw std::out_of_range("Index out of bounds");
+        }
+        return m_m[index(col, row)];
+    }
+    Matrix operator+(const Matrix& other) const {
+        if (m_rows != other.m_rows || m_cols != other.m_cols) {
             throw std::invalid_argument("Matrix size mismatch");
         }
-        Matrix r{m1};
-        for (auto i{0}; i < m1.m_cols * m1.m_rows; ++i) {
-            m1.m_m[i] += m2.m_m[i];
+        Matrix r{ *this };
+        for (auto i{ 0 }; i < m_cols * m_rows; ++i) {
+            r.m_m[i] += other.m_m[i];
         }
         return r;
     }
-    friend Matrix& operator*(const Matrix& m1, const Matrix& m2) {
-        if (m1.m_rows != m2.m_rows || m1.m_cols != m2.m_cols) {
+    Matrix operator*(const Matrix& other) const {
+        if (m_rows != other.m_rows || m_cols != other.m_cols) {
             throw std::invalid_argument("Matrix size mismatch");
         }
-        Matrix r{ m1 };
-        for (auto i{ 0 }; i < m1.m_cols * m1.m_rows; ++i) {
-            m1.m_m[i] *= m2.m_m[i];
+        Matrix r{ *this };
+        for (auto i{ 0 }; i < m_cols * m_rows; ++i) {
+            r.m_m[i] *= other.m_m[i];
         }
         return r;
     }
     // TODO: iterator
 private:
-    size_t m_rows;
-    size_t m_cols;
-    std::unique_ptr<int[]> m_m{};
+    int m_rows;
+    int m_cols;
+    std::unique_ptr<double[]> m_m{};
 
     constexpr inline const int index(int row, int col) const noexcept {
         return row + col * m_cols;
     }
 };
 
+void fill_with_random(Matrix& m, double min_val, double max_val) {
+    static default_random_engine e{};
+    static uniform_real_distribution gen{ min_val, max_val };
+
+    // TODO: update to use iterator instead of conventional array
+    for (auto i{ 0 }; i < m.rows(); ++i) {
+        for (auto j{ 0 }; j < m.cols(); ++j) {
+            m(i, j) = gen(e);
+        }
+    }
+}
+
+void print_matrix(const Matrix& m) {
+    for (auto i{ 0 }; i < m.rows(); ++i) {
+        for (auto j{ 0 }; j < m.cols(); ++j) {
+            std::cout << m(i, j);
+            std::cout << (j < m.cols() - 1 ? " " : "\n");
+        }
+    }
+}
+
 int main() {
+    Matrix m1{3, 2};
+    std::cout << "M1:\n";
+    print_matrix(m1);
+    fill_with_random(m1, 3, 10);
+    std::cout << "M1 after randomizing:\n";
+    print_matrix(m1);
+
+    auto m2(m1);
+    std::cout << "M2 (copy):\n";
+    print_matrix(m2);
+    fill_with_random(m1, 0, 3);
+    std::cout << "M1 after randomizing:\n";
+    print_matrix(m1);
+
+    std::cout << "M1 + M2:\n";
+    print_matrix(m1 + m2);
+
+    std::cout << "M1 * M2:\n";
+    print_matrix(m1 * m2);
+
     return 0;
 }
