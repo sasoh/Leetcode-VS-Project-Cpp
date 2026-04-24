d83bbba3c1f7efd857909c730ca797263462d046
Alexander Popov <lxndrppv@gmail.com>
2026-04-24 14:28:05 +0300

    feat: ex. 5 solution (previous commit was for ex. 4)

--- ConsoleApp/main.cpp
+++ ConsoleApp/main.cpp
@@ -1,25 +1,24 @@
 ﻿#include <iostream>
 #include <format>
 #include <array>
+#include <type_traits>
 using namespace std;
 
-// Write a function template named findInArray that takes an array of type T,
-// the array size, and a value of type T to search for.
-// The function should return the index of the first occurrence of the value in the array,
-// or -1 if the value is not found.
+// Implement a function template named sum that takes an array 
+// of any numeric type(int, float, double) and its size, 
+// and returns the sum of all its elements.
 
-template<typename T>
-int findInArray(const T array[], size_t size, T searched) {
+template<typename T> requires std::is_arithmetic_v<T>
+T sum(const T arr[], size_t size) {
+    T sum{};
     for (int i{0}; i < size; ++i) {
-        if (array[i] == searched) {
-            return i;
-        }
+        sum += arr[i];
     }
-    return -1;
+    return sum;
 }
 
 int main() {
     int a1[]{ 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-    cout << findInArray<int>(a1, 9, 1) << "\n";
+    cout << "Sum = " << sum(a1, 9) << "\n";
     return 0;
 }
