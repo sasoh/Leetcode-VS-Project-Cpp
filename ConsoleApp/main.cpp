53d467a34fc7839a6db0f9565ff3580d9ada9975
Alexander Popov <lxndrppv@gmail.com>
2026-04-24 11:56:05 +0300

    feat: ex. 2 solution

--- ConsoleApp/main.cpp
+++ ConsoleApp/main.cpp
@@ -2,20 +2,21 @@
 #include <format>
 using namespace std;
 
-// Create a function template named myMax that accepts two parameters of the same type 
-// and returns the larger of the two values. The function should work correctly for 
-// standard data types like int, double, and char.
+// Implement a function template named mySwap that takes two parameters by reference, and swaps their values. 
+// Demonstrate its usage with int and float variables.
 
 template<typename T>
-T myMax(const T& a, const T& b) {
-    return a >= b ? a : b;
+void mySwap(T& a, T&b) {
+    T temp{a};
+    a = b;
+    b = temp;
 }
 
 int main() {
-    cout << format("max1 = {}\n", myMax(1, 5));
-    cout << format("max2 = {}\n", myMax(5, 1));
-    cout << format("max3 = {}\n", myMax(2.3, 2.4));
-    cout << format("max4 = {}\n", myMax(2.4, 2.3));
-    cout << format("max5 = {}\n", myMax('a', 'c'));
+    int a1 = 2;
+    int a2 = 3;
+    cout << format("a1 = {} a2 = {}\n", a1, a2);
+    mySwap(a1, a2);
+    cout << format("a1 = {} a2 = {}\n", a1, a2);
     return 0;
 }
