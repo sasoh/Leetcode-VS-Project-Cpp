89dcd8acf8e54b2bee8d0086c0f3a8638918b426
Alexander Popov <lxndrppv@gmail.com>
2026-04-24 17:09:30 +0300

    feat: ex. 13 solution

--- ConsoleApp/main.cpp
+++ ConsoleApp/main.cpp
@@ -1,63 +1,59 @@
-﻿#include <format>
-#include <iostream>
-#include <string>
-#include <vector>
-#include <iterator>
+﻿#include <vector>
 #include <ranges>
+#include <stdexcept>
 #include <algorithm>
+#include <iterator>
+#include <iostream>
 using namespace std;
 
-// Implement a class template named Stack that uses a std::vector internally to store elements of any type T.
-// The stack must support the following public operations:
-//      push(T element), 
-//      T pop() (which removes and returns the top element), 
-//      bool isEmpty() (to check if the stack is empty).
+// Implement a class template named Queue(First - In, First - Out) that can hold elements of any type T.
+// It should expose the following public methods:
+//      enqueue(T element) (add to the back), 
+//      T dequeue() (remove and return the front element), 
+//      bool isEmpty().
+// You may use a standard library container like std::vector or std::list internally.
 
 template<typename T>
-class CustomStack {
+class CustomQueue {
 public:
-    CustomStack() : m_v{} {}
-    void push(T element) {
-        m_v.push_back(element);
+    CustomQueue() : m_q{} {}
+    void enqueue(T element) {
+        m_q.push_back(element);
     }
-    T pop() {
-        auto r = m_v.back();
-        m_v.pop_back();
+    T dequeue() {
+        if (isEmpty()) {
+            throw std::out_of_range("No elements in queue");
+        }
+        T r{ m_q.back() };
+        m_q.pop_back();
         return r;
     }
     bool isEmpty() {
-        return m_v.size() == 0;
+        return m_q.size() == 0;
     }
-    void printStack() {
+    void print() {
         ostream_iterator<T> out{ cout, " " };
-        cout << "Stack contents: ";
-        ranges::copy(m_v, out);
+        cout << "Queue contents = ";
+        ranges::copy(m_q, out);
         cout << "\n";
     }
 private:
-    vector<T> m_v{};
+    vector<T> m_q{};
 };
 
 int main() {
-    CustomStack<int> intStack{};
-    intStack.push(3);
-    intStack.push(2);
-    intStack.push(6);
-    intStack.push(78);
-    intStack.printStack();
-    intStack.pop();
-    intStack.pop();
-    intStack.printStack();
-    intStack.pop();
-    cout << "Empty = " << intStack.isEmpty() << "\n";
-    intStack.printStack();
-    intStack.pop();
-    cout << "Empty = " << intStack.isEmpty() << "\n";
-
-    CustomStack<std::string> stringStack{};
-    stringStack.push("test 1"s);
-    stringStack.push("test 2"s);
-    stringStack.push("test 3"s);
-    stringStack.printStack();
+    CustomQueue<int> q1{};
+    q1.enqueue(3);
+    q1.enqueue(2);
+    q1.enqueue(7);
+    q1.enqueue(1);
+    q1.print();
+    q1.dequeue();
+    q1.dequeue();
+    q1.dequeue();
+    q1.print();
+    cout << "Empty = " << q1.isEmpty() << "\n";
+    q1.dequeue();
+    cout << "Empty = " << q1.isEmpty() << "\n";
     return 0;
 }
