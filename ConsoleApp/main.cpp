0328829de302b490c170b6f68ebb8f7edf9b4e6a
Alexander Popov <lxndrppv@gmail.com>
2026-04-24 14:40:07 +0300

    feat: ex. 7 solution

--- ConsoleApp/main.cpp
+++ ConsoleApp/main.cpp
@@ -1,24 +1,18 @@
 ﻿#include <iostream>
-#include <format>
-#include <array>
-#include <type_traits>
-#include <format>
+#include <string>
 using namespace std;
 
-// Create a function template named compareAndPrint that 
-// takes two parameters of potentially different types, T1 and T2.
-// The function should explicitly convert both parameters to a common type (e.g., double)
-// for comparison and then print which original value was larger.
-template<typename T1, typename T2> requires std::is_arithmetic_v<T1> && std::is_arithmetic_v<T2>
-void compareAndPrint(const T1& a, const T2& b) {
-    auto ad = static_cast<double>(a);
-    auto bd = static_cast<double>(b);
-    cout << format("Comparing {} ({}) and {} ({}): {} is greater.\n", a, ad, b, bd, ad >= bd ? a : b);
+// Write a function template named combine that accepts two values of the same type T
+// and returns the result of applying the addition operator (+) to them. 
+// Test the function with int (which performs addition) and with std::string (which performs concatenation).
+
+template<typename T>
+T combine(const T& a, const T& b) {
+    return a + b;
 }
 
 int main() {
-    compareAndPrint(10, 9.9);
-    compareAndPrint('A', 60);
-    compareAndPrint(100.5, 100.49);
+    cout << "Sum = " << combine(3, 5) << "\n";
+    cout << "Sum = " << combine("efji"s, "grhbfu"s) << "\n";
     return 0;
 }
