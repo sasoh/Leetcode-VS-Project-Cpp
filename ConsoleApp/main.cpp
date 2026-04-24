8956a97883a45ad0a57e8deef0b09a0dd17cf049
Alexander Popov <lxndrppv@gmail.com>
2026-04-24 11:46:19 +0300

    feat: 15.17 solution

--- ConsoleApp/main.cpp
+++ ConsoleApp/main.cpp
@@ -5,13 +5,17 @@
 #include <unordered_set>
 #include <array>
 #include <format>
+#include <concepts>
 
 //15.15 (Function Template) Create a sumRange function template that receives a range of
 //values, sums them and returns the result.Use a range - based for statement and the += op -
 //erator to perform your calculations.Test your code with a container of ints, a container
 //of doubles and a container of strings.
 
-template<typename T, std::ranges::input_range S>
+template<typename T>
+concept SumRangeParam = std::floating_point<T> || std::integral<T>;
+
+template<SumRangeParam T, std::ranges::input_range S>
     requires requires (T sum, std::ranges::range_value_t<S> v) {
     sum += v;
 }
@@ -28,7 +32,7 @@ int main() {
     std::cout << std::format("Sum of a1 = {}\n", sumRange<int>(a1));
     std::vector<double> v2{ 1.0, 2.1, 3.2, 4.3 };
     std::cout << std::format("Sum of v2 = {}\n", sumRange<double>(v2));
-    std::unordered_set<std::string> v3{ "ab", "cd", "ef" };
-    std::cout << std::format("Sum of v3 = {}\n", sumRange<std::string>(v3));
+    //std::unordered_set<std::string> v3{ "ab", "cd", "ef" };
+    //std::cout << std::format("Sum of v3 = {}\n", sumRange<std::string>(v3)); // now invalid since not float/int
     return 0;
 }
