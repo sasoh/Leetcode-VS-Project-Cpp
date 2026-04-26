9df34ec153e24a53d1bfc872d5cdda862074cb07
Alexander Popov <lxndrppv@gmail.com>
2026-04-26 14:41:11 +0300

    feat: ex. 15 solution

--- ConsoleApp/main.cpp
+++ ConsoleApp/main.cpp
@@ -2,42 +2,43 @@
 #include <iostream>
 #include <memory>
 #include <string>
+#include <format>
 using namespace std;
 
-// Design a class template named GenericArray that dynamically allocates an array of type T 
-// and its size is specified in the constructor. The class must include a destructor to manage 
-// memory and overload the subscript operator ([]) for element access.
+// Create a class template named Calculator with two private member variables, num1 and num2, both of type T. 
+// Include a constructor to initialize these members. 
+// The class must provide public member functions for arithmetic operations: 
+//      add(), 
+//      subtract(), 
+//      multiply(), 
+//      divide(),
+// all returning a value of type T.
 
 template<typename T>
-class GenericArray {
+class Calculator {
 public:
-    GenericArray(size_t size) : m_t{ make_unique<T[]>(size) }, m_size{ size } {
-        for (int i{ 0 }; i < size; ++i) {
-            m_t[i] = {};
-        }
+    Calculator(const T num1, const T num2) : m_num1{ num1 }, m_num2{ num2 } {}
+    T add() {
+        return m_num1 + m_num2;
     }
-    T& operator[](int index) {
-        return m_t[index];
+    T subtract() {
+        return m_num1 - m_num2;
     }
-    void print() {
-        ostream_iterator<T> out(cout, " ");
-        cout << "Elements: ";
-        copy(m_t.get(), m_t.get() + m_size, out);
-        cout << "\n";
+    T multiply() {
+        return m_num1 * m_num2;
+    }
+    T divide() {
+        return m_num1 / m_num2;
     }
 private:
-    unique_ptr<T[]> m_t{};
-    size_t m_size;
+    T m_num1;
+    T m_num2;
 };
 
 int main() {
-    GenericArray<int> a1(4);
-    a1.print();
-    a1[1] = 3;
-    a1.print();
-    GenericArray<string> a2(4);
-    a2.print();
-    a2[1] = "test"s;
-    a2.print();
+    Calculator c1(3, 5);
+    cout << format("{}, {}, {}, {}\n", c1.add(), c1.subtract(), c1.multiply(), c1.divide());
+    Calculator c2(7.5, 2.0);
+    cout << format("{}, {}, {}, {}\n", c2.add(), c2.subtract(), c2.multiply(), c2.divide());
     return 0;
 }
