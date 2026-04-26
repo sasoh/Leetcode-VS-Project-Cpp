74efa171896145d255fd043b300fd2b958e8d79b
Alexander Popov <lxndrppv@gmail.com>
2026-04-26 15:19:03 +0300

    feat: ex. 18 solution

--- ConsoleApp/main.cpp
+++ ConsoleApp/main.cpp
@@ -1,27 +1,47 @@
 ﻿#include <memory>
+#include <iostream>
 using namespace std;
 
-// Define a class template for a Node in a singly linked list.
-// The node must hold a value of generic type T and a pointer to the next node(which must be a pointer to a Node<T>).
-// Include a constructor to initialize the value.
+// Design a simple generic container class MyVector<T> that internally uses a dynamic array (T*). 
+// Include basic iterator support by defining a nested type alias iterator as a pointer to T (T*). 
+// Implement the public methods begin() and end() to return the start and end iterators.
 
 template<typename T>
-class Node {
+class MyVector {
 public:
-    Node(T value) : m_value{ value }, m_next{ nullptr } {}
-    void setNext(Node<T>& next) {
-        m_next = make_shared<Node<T>>(next);
+    using iterator = T*;
+    MyVector(int size) : m_size{ size }, m_array{ make_unique<T[]>(size) } {
+        for (int i{ 0 }; i < m_size; ++i) {
+            m_array[i] = T{};
+        }
+    }
+    iterator begin() {
+        return m_array.get();
+    }
+    iterator end() {
+        return m_array.get() + m_size;
     }
 private:
-    T m_value{};
-    shared_ptr<Node<T>> m_next{ nullptr };
+    unique_ptr<T[]> m_array{ nullptr };
+    int m_size;
 };
 
+template<typename T>
+void print(MyVector<T>& vector) {
+    for (const auto& v : vector) {
+        cout << v << " ";
+    }
+    cout << "\n";
+}
+
 int main() {
-    Node<int> n1(2);
-    Node<int> n2(3);
-    Node<int> n3(4);
-    n2.setNext(n1);
-    n3.setNext(n2);
+    MyVector<double> v1{ 4 };
+    auto value = v1.begin();
+    print(v1);
+    *value = 3;
+    print(v1);
+    value += 1;
+    *value = 2;
+    print(v1);
     return 0;
 }
