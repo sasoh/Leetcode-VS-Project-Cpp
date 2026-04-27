690e215a162bf9a530e37a49d111a2b9e624c2c2
Alexander Popov <lxndrppv@gmail.com>
2026-04-27 17:01:25 +0300

    chore: cleanup

--- ConsoleApp/main.cpp
+++ ConsoleApp/main.cpp
@@ -1,22 +1,6 @@
 ﻿#include <iostream>
 using namespace std;
 
-// (C++11 + ) Write a variadic function template called print_all that can accept zero or more arguments of any type and print them all,
-// separated by spaces.
-// You must use a base case and a recursive case to unpack the parameter pack.
-
-void print_all() {
-    cout << "\n";
-}
-
-template<typename T, typename... Rs>
-void print_all(T first, Rs... rest) {
-    cout << first << " ";
-    print_all(rest...);
-}
-
 int main() {
-    print_all(3, 6, 2);
-    print_all(-3.22, 4, "something");
     return 0;
 }
