ab31610b7b1efac2349ca0ec9aa696d6e1eb9bde
Alexander Popov <lxndrppv@gmail.com>
2026-05-11 10:44:28 +0300

    feat: solution to nucleotide count

--- ConsoleApp/main.cpp
+++ ConsoleApp/main.cpp
@@ -1,6 +1,47 @@
-﻿#include <iostream>
+﻿//  Each of us inherits from our biological parents a set of chemical instructions 
+//  known as DNA that influence how our bodies are constructed.
+//  All known life depends on DNA!
+//  
+//  Note: You do not need to understand anything about nucleotides or DNA to complete this exercise.
+//  
+//  DNA is a long chain of other chemicals and the most important are the four nucleotides, adenine, 
+//  cytosine, guanine and thymine. A single DNA chain can contain billions of these four nucleotides 
+//  and the order in which they occur is important! We call the order of these nucleotides in a bit 
+//  of DNA a "DNA sequence".
+//  
+//  We represent a DNA sequence as an ordered collection of these four nucleotides and a common way 
+//  to do that is with a string of characters such as "ATTACG" for a DNA sequence of 6 nucleotides. 
+//  'A' for adenine, 'C' for cytosine, 'G' for guanine, and 'T' for thymine.
+//  
+//  Given a string representing a DNA sequence, count how many of each nucleotide is present. If the 
+//  string contains characters that aren't A, C, G, or T then it is invalid and you should signal an error.
+//  
+//  For example :
+//  
+//  "GATTACA" -> 'A' : 3, 'C' : 1, 'G' : 1, 'T' : 2
+//  "INVALID"->error
+
+#include <iostream>
+#include <map>
+#include <string>
 using namespace std;
 
+namespace nucleotide_count {
+    auto count(std::string input) {
+        std::map<char, int> r{ {'A', 0}, {'C', 0}, {'G', 0}, {'T', 0} };
+        for (const char& c : input) {
+            if (c != 'A' && c != 'C' && c != 'G' && c != 'T') continue;
+            ++r[c];
+        }
+
+        return r;
+    }
+}  // namespace nucleotide_count
+
 int main() {
+    //const auto actual = nucleotide_count::count("");
+    //const auto actual = nucleotide_count::count("AGCTT");
+    //const auto actual = nucleotide_count::count("MUMBO");
+    const auto actual = nucleotide_count::count("AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC");
     return 0;
 }
