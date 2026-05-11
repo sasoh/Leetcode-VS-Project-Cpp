eaf7e2731e7aca7171829a8aa9bf7ec23cd6ac94
Alexander Popov <lxndrppv@gmail.com>
2026-05-11 11:07:48 +0300

    feat: solution to collatz conjecture

--- ConsoleApp/main.cpp
+++ ConsoleApp/main.cpp
@@ -1,34 +1,35 @@
-﻿//  Your task is to determine the RNA complement of a given DNA sequence.
-//  Both DNA and RNA strands are a sequence of nucleotides.
-//  The four nucleotides found in DNA are adenine(A), cytosine(C), guanine(G), and thymine(T).
-//  The four nucleotides found in RNA are adenine(A), cytosine(C), guanine(G), and uracil(U).
-//  Given a DNA strand, its transcribed RNA strand is formed by replacing each nucleotide with its complement :
-//  G->C
-//  C->G
-//  T->A
-//  A->U
+﻿//The rules were deceptively simple. Pick any positive integer.
+//If it's even, divide it by 2.
+//If it's odd, multiply it by 3 and add 1.
+//Then, repeat these steps with the result, continuing indefinitely.
 
 #include <iostream>
 #include <string>
+#include <stdexcept>
 using namespace std;
 
-namespace rna_transcription {
-    char to_rna(char input) {
-        if (input == 'G') return 'C';
-        if (input == 'C') return 'G';
-        if (input == 'T') return 'A';
-        return 'U';
-    }
-
-    std::string to_rna(const std::string& input) {
-        std::string r{};
-        for (const auto&c : input) {
-            r.push_back(to_rna(c));
+namespace collatz_conjecture {
+    int steps(int start) {
+        if (start < 1) {
+            throw std::domain_error("Number must be more than 1");
         }
-        return r;
+        if (start == 1) {
+            return 0;
+        }
+        if (start % 2 == 0) {
+            return 1 + steps(start / 2);
+        }
+        else {
+            return 1 + steps(3 * start + 1);
+        }
     }
-}  // namespace rna_transcription
+}  // namespace collatz_conjecture
+
 
 int main() {
+    auto a = collatz_conjecture::steps(16);
+    auto b = collatz_conjecture::steps(12);
+    auto c = collatz_conjecture::steps(100000);
+    auto d = collatz_conjecture::steps(0);
     return 0;
 }
