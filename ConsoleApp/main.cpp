efdac98b16766b1521cb69c2d722a3ee677ba60f
Alexander Popov <lxndrppv@gmail.com>
2026-05-11 10:50:07 +0300

    feat: solution to rna transcription

--- ConsoleApp/main.cpp
+++ ConsoleApp/main.cpp
@@ -1,47 +1,34 @@
-﻿//  Each of us inherits from our biological parents a set of chemical instructions 
-//  known as DNA that influence how our bodies are constructed.
-//  All known life depends on DNA!
-//  
-//  Note: You do not need to understand anything about nucleotides or DNA to complete this exercise.
-//  
-//  DNA is a long chain of other chemicals and the most important are the four nucleotides, adenine, 
-//  cytosine, guanine and thymine. A single DNA chain can contain billions of these four nucleotides 
-//  and the order in which they occur is important! We call the order of these nucleotides in a bit 
-//  of DNA a "DNA sequence".
-//  
-//  We represent a DNA sequence as an ordered collection of these four nucleotides and a common way 
-//  to do that is with a string of characters such as "ATTACG" for a DNA sequence of 6 nucleotides. 
-//  'A' for adenine, 'C' for cytosine, 'G' for guanine, and 'T' for thymine.
-//  
-//  Given a string representing a DNA sequence, count how many of each nucleotide is present. If the 
-//  string contains characters that aren't A, C, G, or T then it is invalid and you should signal an error.
-//  
-//  For example :
-//  
-//  "GATTACA" -> 'A' : 3, 'C' : 1, 'G' : 1, 'T' : 2
-//  "INVALID"->error
+﻿//  Your task is to determine the RNA complement of a given DNA sequence.
+//  Both DNA and RNA strands are a sequence of nucleotides.
+//  The four nucleotides found in DNA are adenine(A), cytosine(C), guanine(G), and thymine(T).
+//  The four nucleotides found in RNA are adenine(A), cytosine(C), guanine(G), and uracil(U).
+//  Given a DNA strand, its transcribed RNA strand is formed by replacing each nucleotide with its complement :
+//  G->C
+//  C->G
+//  T->A
+//  A->U
 
 #include <iostream>
-#include <map>
 #include <string>
 using namespace std;
 
-namespace nucleotide_count {
-    auto count(std::string input) {
-        std::map<char, int> r{ {'A', 0}, {'C', 0}, {'G', 0}, {'T', 0} };
-        for (const char& c : input) {
-            if (c != 'A' && c != 'C' && c != 'G' && c != 'T') continue;
-            ++r[c];
-        }
+namespace rna_transcription {
+    char to_rna(char input) {
+        if (input == 'G') return 'C';
+        if (input == 'C') return 'G';
+        if (input == 'T') return 'A';
+        return 'U';
+    }
 
+    std::string to_rna(const std::string& input) {
+        std::string r{};
+        for (const auto&c : input) {
+            r.push_back(to_rna(c));
+        }
         return r;
     }
-}  // namespace nucleotide_count
+}  // namespace rna_transcription
 
 int main() {
-    //const auto actual = nucleotide_count::count("");
-    //const auto actual = nucleotide_count::count("AGCTT");
-    //const auto actual = nucleotide_count::count("MUMBO");
-    const auto actual = nucleotide_count::count("AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC");
     return 0;
 }
