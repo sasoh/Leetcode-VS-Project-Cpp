05af3bb55da7eadf9be07d774cc807e084c8e9af
Alexander Popov <lxndrppv@gmail.com>
2026-05-14 11:31:37 +0300

    chore: cleanup

--- ConsoleApp/main.cpp
+++ ConsoleApp/main.cpp
@@ -13,62 +13,56 @@
 #include <map>
 using namespace std;
 
-
-namespace say {
-    static const std::map<int, std::string> dtw{
-        {1, "one"s}, {2, "two"s}, {3, "three"s}, {4, "four"s}, {5, "five"s},
-        {6, "six"s}, {7, "seven"s}, {8, "eight"s}, {9, "nine"s},
-        {10, "ten"s}, {11, "eleven"s}, {12, "twelve"s}, {13, "thirteen"s}, {14, "fourteen"s},
-        {15, "fifteen"s}, {16, "sixteen"s}, {17, "seventeen"s}, {18, "eighteen"s}, {19, "nineteen"s},
-        {20, "twenty"s}, {30, "thirty"s}, {40, "forty"s}, {50, "fifty"s}, {60, "sixty"s},
-        {70, "seventy"s}, {80, "eighty"s}, {90, "ninety"s},
+namespace {
+    const std::map<int, std::string> dtw{
+        {1, "one"s}, {2, "two"s}, {3, "three"s}, {4, "four"s}, {5, "five"s}, {6, "six"s},
+        {7, "seven"s}, {8, "eight"s}, {9, "nine"s}, {10, "ten"s}, {11, "eleven"s}, {12, "twelve"s},
+        {13, "thirteen"s}, {14, "fourteen"s}, {15, "fifteen"s}, {16, "sixteen"s}, {17, "seventeen"s},
+        {18, "eighteen"s}, {19, "nineteen"s}, {20, "twenty"s}, {30, "thirty"s}, {40, "forty"s},
+        {50, "fifty"s}, {60, "sixty"s}, {70, "seventy"s}, {80, "eighty"s}, {90, "ninety"s},
     };
 
+    void prependSpaceAppend(std::string& target, const std::string& addition) {
+        if (target.size() > 0) {
+            target.append(" "s);
+        }
+        target.append(addition);
+    }
 
-    std::string digitToWord(int d) {
+    std::string threeDigitsToWords(int d) {
         std::string r{};
         if (d == 0) return r;
 
-        // hundreds
         int hundreds = d / 100;
         if (hundreds > 0) {
             r.append(dtw.at(hundreds) + " hundred"s);
         }
 
-        // tenths
         int tenths = (d / 10) % 10;
         int singles = d % 10;
         if (tenths > 0) {
-            // check for whole two-digit number
             int twoDigits = d % 100;
-            if (dtw.find(twoDigits) != dtw.end()) {
-                if (r.size() > 0) {
-                    r.append(" "s);
-                }
-                r.append(dtw.at(twoDigits));
+            if (auto it = dtw.find(twoDigits); it != dtw.end()) {
+                prependSpaceAppend(r, it->second);
             }
             else {
-                if (r.size() > 0) {
-                    r.append(" "s);
-                }
-                r.append(dtw.at(tenths * 10) + "-" + dtw.at(singles));
+                prependSpaceAppend(r, dtw.at(tenths * 10) + "-"s + dtw.at(singles));
             }
         }
         else {
             if (singles > 0) {
-                if (r.size() > 0) {
-                    r.append(" "s);
-                }
-                r.append(dtw.at(singles));
+                prependSpaceAppend(r, dtw.at(singles));
             }
         }
 
         return r;
     }
+}
 
+namespace say {
     std::string in_english(long long n) {
-        if (n > 999'999'999'999) {
-            throw std::domain_error("N should be smaller");
+        if (n < 0 || n > 999'999'999'999) {
+            throw std::domain_error("n is not in domain");
         }
         if (n == 0) {
             return "zero"s;
@@ -83,42 +77,10 @@ namespace say {
         };
         for (const auto& [v, suffix] : p) {
             if (v < 1) continue;
-            auto words = digitToWord(v);
-            if (r.size() > 0) {
-                r.append(" "s);
-            }
-            r += words + suffix;
+            auto words = threeDigitsToWords(v);
+            prependSpaceAppend(r, words + suffix);
         } 
 
-        //int billions = (n / 1'000'000'000) % 1000;
-        //if (billions > 0) {
-        //    auto words = digitToWord(billions);
-        //    r += words + " billion";
-        //}
-        //int millions = (n / 1'000'000) % 1000;
-        //if (millions > 0) {
-        //    auto words = digitToWord(millions);
-        //    if (r.size() > 0) {
-        //        r.append(" "s);
-        //    }
-        //    r += words + " million";
-        //}
-        //int thousands = (n / 1000) % 1000;
-        //if (thousands > 0) {
-        //    auto words = digitToWord(thousands);
-        //    if (r.size() > 0) {
-        //        r.append(" "s);
-        //    }
-        //    r += words + " thousand";
-        //}
-        //int hundreds = n % 1000;
-        //if (hundreds > 0) {
-        //    auto words = digitToWord(hundreds);
-        //    if (r.size() > 0) {
-        //        r.append(" "s);
-        //    }
-        //    r += words;
-        //}
         return r;
     }
 }  // namespace say
