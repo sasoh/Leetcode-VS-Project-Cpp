0f42904d42e4d0fa028004250cc0b23d4250219b
Alexander Popov <lxndrppv@gmail.com>
2026-05-17 14:38:44 +0300

    feat: solution to protein translation

--- ConsoleApp/main.cpp
+++ ConsoleApp/main.cpp
@@ -1,90 +1,69 @@
-﻿//  An allergy test produces a single numeric score which contains the information about all the allergies 
-//  the person has (that they were tested for).
-//  The list of items (and their value) that were tested are:
-//  eggs(1)
-//  peanuts(2)
-//  shellfish(4)
-//  strawberries(8)
-//  tomatoes(16)
-//  chocolate(32)
-//  pollen(64)
-//  cats(128)
-//  So if Tom is allergic to peanuts and chocolate, he gets a score of 34.
-//  Now, given just that score of 34, your program should be able to say:
-//      Whether Tom is allergic to any one of those allergens listed above.
-//      All the allergens Tom is allergic to.
-//  Note: a given score may include allergens not listed above (i.e.allergens that score 256, 512, 1024, etc.).
-//  Your program should ignore those components of the score. For example, if the allergy score is 257, your 
-//  program should only report the eggs(1) allergy.
+﻿//  Codon	            Amino Acid
+//  AUG	                Methionine
+//  UUU, UUC	        Phenylalanine
+//  UUA, UUG	        Leucine
+//  UCU, UCC, UCA, UCG	Serine
+//  UAU, UAC	        Tyrosine
+//  UGU, UGC	        Cysteine
+//  UGG	                Tryptophan
+//  UAA, UAG, UGA	    STOP
+//  If you encounter any of the stop codons, ignore the rest of the sequence — the protein is complete.
 
 #include <iostream>
 #include <string>
-#include <unordered_set>
-#include <bitset>
-#include <cmath>
-#include <cstdint>
-#include <format>
+#include <vector>
+#include <map>
 
 using namespace std::string_literals;
 
 namespace {
-    static const std::string allergens[]{
-        "eggs"s,
-        "peanuts"s,
-        "shellfish"s,
-        "strawberries"s,
-        "tomatoes"s,
-        "chocolate"s,
-        "pollen"s,
-        "cats"s,
+    using std::string;
+    using std::map;
+    static map<string, string> ctoa{
+        {"AUG"s, "Methionine"},
+        {"UUU"s, "Phenylalanine"},
+        {"UUC"s, "Phenylalanine"},
+        {"UUA"s, "Leucine"},
+        {"UUG"s, "Leucine"},
+        {"UCU"s, "Serine"},
+        {"UCC"s, "Serine"},
+        {"UCA"s, "Serine"},
+        {"UCG"s, "Serine"},
+        {"UAU"s, "Tyrosine"},
+        {"UAC"s, "Tyrosine"},
+        {"UGU"s, "Cysteine"},
+        {"UGC"s, "Cysteine"},
+        {"UGG"s, "Tryptophan"}
     };
+
+    static bool isStop(const string& s) {
+        return s == "UAA"s || s == "UAG"s || s == "UGA"s;
+    }
 }
 
-namespace allergies {
-    class allergy_test {
-    public:
-        explicit allergy_test(unsigned int level) : m_level{ level <= 256 ? level : level % 256 } {
-            std::cout << std::format("Set m_level to {}\n", m_level.to_string());
-        }
-        bool is_allergic_to(const std::string& item) const;
-        std::unordered_set<std::string> get_allergies() const;
-    private:
-        std::bitset<8> m_level{};
-    };
+namespace protein_translation {
+    using std::string;
+    using std::vector;
 
-    bool allergy_test::is_allergic_to(const std::string& item) const
-    {
-        for (size_t i{ 0 }; i < 8; ++i) {
-            if (allergens[i] == item && m_level[i]) {
-                return true;
+    auto proteins(const string& input) {
+        vector<string> r{};
+
+        for (auto i{ 0 }; i < input.size(); i += 3)
+        {
+            auto substring = input.substr(i, 3);
+            if (isStop(substring)) {
+                break;
             }
+            r.push_back(ctoa[substring]);
         }
-        return false;
-    }
-    std::unordered_set<std::string> allergy_test::get_allergies() const
-    {
-        std::unordered_set<std::string> r{};
-        for (size_t i{ 0 }; i < 8; ++i) {
-            if (m_level[i]) {
-                r.emplace(allergens[i]);
-            }
-        }
+
         return r;
     }
-}  // namespace allergies
+}  // namespace protein_translation
 
 int main() {
-    //auto a1 = allergies::allergy_test(55);
-    //std::cout << a1.is_allergic_to("eggs") << std::endl;
-    //std::cout << a1.is_allergic_to("peanuts") << std::endl;
-    //std::cout << a1.is_allergic_to("shellfish") << std::endl;
-    //std::cout << a1.is_allergic_to("strawberries") << std::endl;
-    //auto a2 = allergies::allergy_test(505);
-    //auto a3 = allergies::allergy_test(5500);
-    //auto a = allergies::allergy_test(32);
-    //auto b = a.is_allergic_to("chocolate");
-    //auto a = allergies::allergy_test(128);
-    auto a = allergies::allergy_test(192);
-    //auto b = a.is_allergic_to("cats");
+    //std::vector<std::string> p = protein_translation::proteins("AUG");
+    //std::vector<std::string> expected{ "Methionine" };
+    protein_translation::proteins("UGGUGUUAUUAAUGGUUU");
     return 0;
 }
