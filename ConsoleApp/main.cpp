0dd70e58f04624c9ce726415b50362baaea7142a
Alexander Popov <lxndrppv@gmail.com>
2026-05-17 14:55:38 +0300

    feat: solution to sum of multiples

--- ConsoleApp/main.cpp
+++ ConsoleApp/main.cpp
@@ -1,69 +1,43 @@
-﻿//  Codon	            Amino Acid
-//  AUG	                Methionine
-//  UUU, UUC	        Phenylalanine
-//  UUA, UUG	        Leucine
-//  UCU, UCC, UCA, UCG	Serine
-//  UAU, UAC	        Tyrosine
-//  UGU, UGC	        Cysteine
-//  UGG	                Tryptophan
-//  UAA, UAG, UGA	    STOP
-//  If you encounter any of the stop codons, ignore the rest of the sequence — the protein is complete.
+﻿//  The energy points are awarded according to the following rules:
+//  
+//      1. For each magical item, take the base value and find all the multiples of that value that are less than the level number.
+//      2. Combine the sets of numbers.
+//      3. Remove any duplicates.
+//      4. Calculate the sum of all the numbers that are left.
 
-#include <iostream>
-#include <string>
-#include <vector>
-#include <map>
-
-using namespace std::string_literals;
+#include <initializer_list>
+#include <numeric>
+#include <set>
 
 namespace {
-    using std::string;
-    using std::map;
-    static map<string, string> ctoa{
-        {"AUG"s, "Methionine"},
-        {"UUU"s, "Phenylalanine"},
-        {"UUC"s, "Phenylalanine"},
-        {"UUA"s, "Leucine"},
-        {"UUG"s, "Leucine"},
-        {"UCU"s, "Serine"},
-        {"UCC"s, "Serine"},
-        {"UCA"s, "Serine"},
-        {"UCG"s, "Serine"},
-        {"UAU"s, "Tyrosine"},
-        {"UAC"s, "Tyrosine"},
-        {"UGU"s, "Cysteine"},
-        {"UGC"s, "Cysteine"},
-        {"UGG"s, "Tryptophan"}
-    };
+    auto multiplesBelow(int of, int limit) {
+        std::set<int> m{};
 
-    static bool isStop(const string& s) {
-        return s == "UAA"s || s == "UAG"s || s == "UGA"s;
+        int nextMultiple = 1;
+        while (of * nextMultiple < limit) {
+            m.insert(of * nextMultiple);
+            ++nextMultiple;
+        }
+
+        return m;
     }
 }
 
-namespace protein_translation {
-    using std::string;
-    using std::vector;
+namespace sum_of_multiples {
+    int to(std::initializer_list<int> items, int level) {
+        std::set<int> m{};
 
-    auto proteins(const string& input) {
-        vector<string> r{};
-
-        for (auto i{ 0 }; i < input.size(); i += 3)
-        {
-            auto substring = input.substr(i, 3);
-            if (isStop(substring)) {
-                break;
-            }
-            r.push_back(ctoa[substring]);
+        for (auto i {items.begin()}; i != items.end(); ++i) {
+            auto multiples = multiplesBelow(*i, level);
+            m.insert(multiples.begin(), multiples.end());
         }
 
-        return r;
+        return std::accumulate(m.begin(), m.end(), 0);
     }
-}  // namespace protein_translation
+
+}  // namespace sum_of_multiples
 
 int main() {
-    //std::vector<std::string> p = protein_translation::proteins("AUG");
-    //std::vector<std::string> expected{ "Methionine" };
-    protein_translation::proteins("UGGUGUUAUUAAUGGUUU");
+
     return 0;
 }
