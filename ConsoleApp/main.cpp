7c1bdfe1929ed274ecc7d1375f84a44767e58550
Alexander Popov <lxndrppv@gmail.com>
2026-05-26 11:01:43 +0300

    feat: solution to prime factors

--- ConsoleApp/main.cpp
+++ ConsoleApp/main.cpp
@@ -1,94 +1,56 @@
-﻿#include <string>
-#include <sstream>
-#include <iomanip>
+﻿//Compute the prime factors of a given natural number.
+//A prime number is only evenly divisible by itself and 1.
+//Note that 1 is not a prime number.
 #include <iostream>
-#include <format>
+#include <vector>
+#include <utility>
 
-using namespace std::string_literals;
+using std::vector;
+using std::pair;
 
-namespace {
-    int toMinutes(int h, int m) {
-        return h * 60 + m;
+namespace prime_factors {
+    vector<long long> of(long long number) {
+        vector<long long> r{};
+        auto n = number;
+        long long f{2};
+        while (n > 1) {
+            if (n % f != 0) {
+                f++;
+            }
+            else {
+                n /= f;
+                r.push_back(f);
+            }
+        }
+        return r;
     }
-    int hours(int m) {
-        if (m > 0) {
-            return (m / 60) % 24;
-        }
-
-        int hours = m / 60;
-        if (m % 60 != 0) {
-            hours--;
-        }
-        int wrappedNegative = hours % 24;
-        int positiveHours = 24 + wrappedNegative;
-        int wrappedPositive = positiveHours % 24;
-        return wrappedPositive;
-    }
-    int minutes(int m) {
-        if (m > 0) {
-            return m % 60;
-        }
-        else {
-            return (60 + m % 60) % 60;
-        }
-    }
-}
-
-namespace date_independent {
-    class clock {
-    public:
-        clock(int h, int m);
-        static clock at(int h, int m);
-        clock plus(int m);
-        bool operator==(const clock& other) const {
-            return (&other == this) || (m_h == other.m_h && m_m == other.m_m);
-        }
-        bool operator!=(const clock& other) const {
-            return !operator==(other);
-        }
-        explicit operator std::string() const noexcept {
-            std::ostringstream oss;
-            oss << std::setfill('0') << std::setw(2) << m_h << ":" << std::setw(2) << m_m;
-            return oss.str();
-        }
-    private:
-        int m_h{};
-        int m_m{};
-    };
-
-    clock::clock(int h, int m)
-    {
-        int total = toMinutes(h, m);
-        m_h = hours(total);
-        m_m = minutes(total);
-    }
-
-    clock clock::at(int h, int m)
-    {
-        return clock{ h, m };
-    }
-    clock clock::plus(int m)
-    {
-        int total = toMinutes(m_h, m_m) + m;
-        m_h = hours(total);
-        m_m = minutes(total);
-        return *this;
-    }
-}  // namespace date_independent
+}  // namespace prime_factors
 
 int main() {
-    //auto c = date_independent::clock::at(1, -40);
-    //auto c = date_independent::clock::at(0, 45).plus(40);
-    //{ 10, 3, -30, "09:33", "subtract to previous hour" },
-    //auto c = date_independent::clock::at(10, 3).plus(-30);
-    //std::cout << std::format("{}: {}\n", "09:33"s, static_cast<std::string>(c));
-    //{ 10, 3, -70, "08:53", "subtract more than an hour" },
-    //{ -1, 15, "23:15", "negative hour" },
-    //auto c = date_independent::clock::at(-1, 15);
-    //std::cout << std::format("{}: {}\n", "23:15"s, static_cast<std::string>(c));
-    //{ -25, 0, "23:00", "negative hour rolls over" },
-    std::cout << std::format("{}: {}\n", "23:00"s, static_cast<std::string>(date_independent::clock::at(-25, 0)));
-    //{ 1, -160, "22:20", "negative minutes roll over" },
-    std::cout << std::format("{}: {}\n", "22:20"s, static_cast<std::string>(date_independent::clock::at(1, -160)));
+    vector<pair<long long, vector<long long>>> tests{
+        {1, {}},
+        {2, {2}},
+        {3, {3}},
+        {9, {3, 3}},
+        {4, {2, 2}},
+        {8, {2, 2, 2}},
+        {27, {3, 3, 3}},
+        {625, {5, 5, 5, 5}},
+        {6, {2, 3}},
+        {12, {2, 2, 3}},
+        {901255, {5, 17, 23, 461}},
+        {93819012551, {11, 9539, 894119}}
+    };
+
+    for (const auto& [i, expected] : tests) {
+        const auto& f = prime_factors::of(i);
+        if (f == expected) {
+            std::cout << "Correct result for " << i << "\n";
+        }
+        else {
+            std::cout << "Incorrect result for " << i << "\n";
+        }
+    }
+ 
     return 0;
 }
