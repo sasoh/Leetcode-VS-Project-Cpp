71c2f13990ef4d6918cd2d03fec220e3d141bdb5
Alexander Popov <lxndrppv@gmail.com>
2026-05-28 20:04:06 +0300

    feat: solution to meetup

--- ConsoleApp/main.cpp
+++ ConsoleApp/main.cpp
@@ -1,62 +1,274 @@
-﻿//  The first time you turn on a robot, a random name is generated in the format of 
-//  two uppercase letters followed by three digits, such as RX837 or BC811.
-//  Every once in a while we need to reset a robot to its factory settings, which means 
-//  that its name gets wiped. The next time you ask, that robot will respond with a new random name.
-//  The names must be random: they should not follow a predictable sequence.
-//  Using random names means a risk of collisions. Your solution must ensure that every existing robot has a unique name.
-#include <iostream>
-#include <string>
+﻿#include <iostream>
 #include <format>
-#include <random>
-#include <sstream>
-#include <iomanip>
+using std::format;
+using std::cout;
+using std::endl;
 
-namespace robot_name {
-    class robot {
+#include <boost/date_time/gregorian/gregorian.hpp>
+#include <boost/date_time/gregorian/greg_weekday.hpp>
+#include <boost/date_time/date_defs.hpp>
+
+namespace meetup {
+    class scheduler {
     public:
-        robot();
-        std::string name() const;
-        void reset();
+        scheduler(boost::gregorian::months_of_year month, int year);
+        boost::gregorian::date monteenth() const;
+        boost::gregorian::date tuesteenth() const;
+        boost::gregorian::date wednesteenth() const;
+        boost::gregorian::date thursteenth() const;
+        boost::gregorian::date friteenth() const;
+        boost::gregorian::date saturteenth() const;
+        boost::gregorian::date sunteenth() const;
+        boost::gregorian::date first_monday() const;
+        boost::gregorian::date first_tuesday() const;
+        boost::gregorian::date first_wednesday() const;
+        boost::gregorian::date first_thursday() const;
+        boost::gregorian::date first_friday() const;
+        boost::gregorian::date first_saturday() const;
+        boost::gregorian::date first_sunday() const;
+        boost::gregorian::date second_monday() const;
+        boost::gregorian::date second_tuesday() const;
+        boost::gregorian::date second_wednesday() const;
+        boost::gregorian::date second_thursday() const;
+        boost::gregorian::date second_friday() const;
+        boost::gregorian::date second_saturday() const;
+        boost::gregorian::date second_sunday() const;
+        boost::gregorian::date third_monday() const;
+        boost::gregorian::date third_tuesday() const;
+        boost::gregorian::date third_wednesday() const;
+        boost::gregorian::date third_thursday() const;
+        boost::gregorian::date third_friday() const;
+        boost::gregorian::date third_saturday() const;
+        boost::gregorian::date third_sunday() const;
+        boost::gregorian::date fourth_monday() const;
+        boost::gregorian::date fourth_tuesday() const;
+        boost::gregorian::date fourth_wednesday() const;
+        boost::gregorian::date fourth_thursday() const;
+        boost::gregorian::date fourth_friday() const;
+        boost::gregorian::date fourth_saturday() const;
+        boost::gregorian::date fourth_sunday() const;
+        boost::gregorian::date last_monday() const;
+        boost::gregorian::date last_tuesday() const;
+        boost::gregorian::date last_wednesday() const;
+        boost::gregorian::date last_thursday() const;
+        boost::gregorian::date last_friday() const;
+        boost::gregorian::date last_saturday() const;
+        boost::gregorian::date last_sunday() const;
     private:
-        std::string m_name{};
-        std::default_random_engine m_rengine{};
+        boost::gregorian::date m_date;
     };
 
-    robot::robot() : m_rengine{ (std::random_device())() } {
-        reset();
-    }
-    std::string robot::name() const {
-        return m_name;
-    }
-    void robot::reset() {
-        std::uniform_int_distribution letterDistribution{ 0, 25 };
-        std::uniform_int_distribution numberDistribution{ 0, 999 };
+    namespace {
+        boost::gregorian::date find_first_teenth_day(boost::gregorian::date from, boost::gregorian::greg_weekday::weekday_enum day)
+        {
+            for (size_t i{ 12 }; i < 20; ++i) {
+                auto newDay = from + boost::gregorian::date_duration(i);
+                if (newDay.day_of_week() == day) {
+                    return newDay;
+                }
+            }
+            return boost::gregorian::date{};
+        }
 
-        char letter1 = static_cast<char>(static_cast<int>('A') + letterDistribution(m_rengine)); 
-        char letter2 = static_cast<char>(static_cast<int>('A') + letterDistribution(m_rengine)); 
-        int number = numberDistribution(m_rengine);
+        boost::gregorian::date find_first_day(boost::gregorian::date from, boost::gregorian::greg_weekday::weekday_enum day, int skipFirst = 0)
+        {
+            for (size_t i{ 0 }; i < 31; ++i) {
+                auto newDay = from + boost::gregorian::date_duration(i);
+                if (newDay.day_of_week() == day && (skipFirst-- < 1)) {
+                    return newDay;
+                }
+            }
+            return boost::gregorian::date{};
+        }
 
-        std::ostringstream ss{};
-        ss << letter1 << letter2;
-        ss << std::setw(3) << std::setfill('0') << number;
-        m_name = ss.str();
+        boost::gregorian::date find_last_teenth_day(boost::gregorian::date from, boost::gregorian::greg_weekday::weekday_enum day)
+        {
+            boost::gregorian::date r{};
+            for (size_t i{ 0 }; i < boost::gregorian::gregorian_calendar::end_of_month_day(from.year(), from.month()); ++i) {
+                auto newDay = from + boost::gregorian::date_duration(i);
+                if (newDay.day_of_week() == day) {
+                    r = newDay;
+                }
+            }
+            return r;
+        }
     }
-}  // namespace robot_name
+
+    scheduler::scheduler(boost::gregorian::months_of_year month, int year) : m_date{ boost::gregorian::date(year, month, 1) } {}
+
+    boost::gregorian::date scheduler::monteenth() const
+    {
+        return find_first_teenth_day(m_date, boost::gregorian::greg_weekday::weekday_enum::Monday);
+    }
+    boost::gregorian::date scheduler::tuesteenth() const
+    {
+        return find_first_teenth_day(m_date, boost::gregorian::greg_weekday::weekday_enum::Tuesday);
+    }
+    boost::gregorian::date scheduler::wednesteenth() const
+    {
+        return find_first_teenth_day(m_date, boost::gregorian::greg_weekday::weekday_enum::Wednesday);
+    }
+    boost::gregorian::date scheduler::thursteenth() const
+    {
+        return find_first_teenth_day(m_date, boost::gregorian::greg_weekday::weekday_enum::Thursday);
+    }
+    boost::gregorian::date scheduler::friteenth() const
+    {
+        return find_first_teenth_day(m_date, boost::gregorian::greg_weekday::weekday_enum::Friday);
+    }
+    boost::gregorian::date scheduler::saturteenth() const
+    {
+        return find_first_teenth_day(m_date, boost::gregorian::greg_weekday::weekday_enum::Saturday);
+    }
+    boost::gregorian::date scheduler::sunteenth() const
+    {
+        return find_first_teenth_day(m_date, boost::gregorian::greg_weekday::weekday_enum::Sunday);
+    }
+    boost::gregorian::date scheduler::first_monday() const
+    {
+        return find_first_day(m_date, boost::gregorian::greg_weekday::weekday_enum::Monday);
+    }
+    boost::gregorian::date scheduler::first_tuesday() const
+    {
+        return find_first_day(m_date, boost::gregorian::greg_weekday::weekday_enum::Tuesday);
+    }
+    boost::gregorian::date scheduler::first_wednesday() const
+    {
+        return find_first_day(m_date, boost::gregorian::greg_weekday::weekday_enum::Wednesday);
+    }
+    boost::gregorian::date scheduler::first_thursday() const
+    {
+        return find_first_day(m_date, boost::gregorian::greg_weekday::weekday_enum::Thursday);
+    }
+    boost::gregorian::date scheduler::first_friday() const
+    {
+        return find_first_day(m_date, boost::gregorian::greg_weekday::weekday_enum::Friday);
+    }
+    boost::gregorian::date scheduler::first_saturday() const
+    {
+        return find_first_day(m_date, boost::gregorian::greg_weekday::weekday_enum::Saturday);
+    }
+    boost::gregorian::date scheduler::first_sunday() const
+    {
+        return find_first_day(m_date, boost::gregorian::greg_weekday::weekday_enum::Sunday);
+    }
+    boost::gregorian::date scheduler::second_monday() const
+    {
+        return find_first_day(m_date, boost::gregorian::greg_weekday::weekday_enum::Monday, 1);
+    }
+    boost::gregorian::date scheduler::second_tuesday() const
+    {
+        return find_first_day(m_date, boost::gregorian::greg_weekday::weekday_enum::Tuesday, 1);
+    }
+    boost::gregorian::date scheduler::second_wednesday() const
+    {
+        return find_first_day(m_date, boost::gregorian::greg_weekday::weekday_enum::Wednesday, 1);
+    }
+    boost::gregorian::date scheduler::second_thursday() const
+    {
+        return find_first_day(m_date, boost::gregorian::greg_weekday::weekday_enum::Thursday, 1);
+    }
+    boost::gregorian::date scheduler::second_friday() const
+    {
+        return find_first_day(m_date, boost::gregorian::greg_weekday::weekday_enum::Friday, 1);
+    }
+    boost::gregorian::date scheduler::second_saturday() const
+    {
+        return find_first_day(m_date, boost::gregorian::greg_weekday::weekday_enum::Saturday, 1);
+    }
+    boost::gregorian::date scheduler::second_sunday() const
+    {
+        return find_first_day(m_date, boost::gregorian::greg_weekday::weekday_enum::Sunday, 1);
+    }
+    boost::gregorian::date scheduler::third_monday() const
+    {
+        return find_first_day(m_date, boost::gregorian::greg_weekday::weekday_enum::Monday, 2);
+    }
+    boost::gregorian::date scheduler::third_tuesday() const
+    {
+        return find_first_day(m_date, boost::gregorian::greg_weekday::weekday_enum::Tuesday, 2);
+    }
+    boost::gregorian::date scheduler::third_wednesday() const
+    {
+        return find_first_day(m_date, boost::gregorian::greg_weekday::weekday_enum::Wednesday, 2);
+    }
+    boost::gregorian::date scheduler::third_thursday() const
+    {
+        return find_first_day(m_date, boost::gregorian::greg_weekday::weekday_enum::Thursday, 2);
+    }
+    boost::gregorian::date scheduler::third_friday() const
+    {
+        return find_first_day(m_date, boost::gregorian::greg_weekday::weekday_enum::Friday, 2);
+    }
+    boost::gregorian::date scheduler::third_saturday() const
+    {
+        return find_first_day(m_date, boost::gregorian::greg_weekday::weekday_enum::Saturday, 2);
+    }
+    boost::gregorian::date scheduler::third_sunday() const
+    {
+        return find_first_day(m_date, boost::gregorian::greg_weekday::weekday_enum::Sunday, 2);
+    }
+    boost::gregorian::date scheduler::fourth_monday() const
+    {
+        return find_first_day(m_date, boost::gregorian::greg_weekday::weekday_enum::Monday, 3);
+    }
+    boost::gregorian::date scheduler::fourth_tuesday() const
+    {
+        return find_first_day(m_date, boost::gregorian::greg_weekday::weekday_enum::Tuesday, 3);
+    }
+    boost::gregorian::date scheduler::fourth_wednesday() const
+    {
+        return find_first_day(m_date, boost::gregorian::greg_weekday::weekday_enum::Wednesday, 3);
+    }
+    boost::gregorian::date scheduler::fourth_thursday() const
+    {
+        return find_first_day(m_date, boost::gregorian::greg_weekday::weekday_enum::Thursday, 3);
+    }
+    boost::gregorian::date scheduler::fourth_friday() const
+    {
+        return find_first_day(m_date, boost::gregorian::greg_weekday::weekday_enum::Friday, 3);
+    }
+    boost::gregorian::date scheduler::fourth_saturday() const
+    {
+        return find_first_day(m_date, boost::gregorian::greg_weekday::weekday_enum::Saturday, 3);
+    }
+    boost::gregorian::date scheduler::fourth_sunday() const
+    {
+        return find_first_day(m_date, boost::gregorian::greg_weekday::weekday_enum::Sunday, 3);
+    }
+    boost::gregorian::date scheduler::last_monday() const
+    {
+        return find_last_teenth_day(m_date, boost::gregorian::greg_weekday::weekday_enum::Monday);
+    }
+    boost::gregorian::date scheduler::last_tuesday() const
+    {
+        return find_last_teenth_day(m_date, boost::gregorian::greg_weekday::weekday_enum::Tuesday);
+    }
+    boost::gregorian::date scheduler::last_wednesday() const
+    {
+        return find_last_teenth_day(m_date, boost::gregorian::greg_weekday::weekday_enum::Wednesday);
+    }
+    boost::gregorian::date scheduler::last_thursday() const
+    {
+        return find_last_teenth_day(m_date, boost::gregorian::greg_weekday::weekday_enum::Thursday);
+    }
+    boost::gregorian::date scheduler::last_friday() const
+    {
+        return find_last_teenth_day(m_date, boost::gregorian::greg_weekday::weekday_enum::Friday);
+    }
+    boost::gregorian::date scheduler::last_saturday() const
+    {
+        return find_last_teenth_day(m_date, boost::gregorian::greg_weekday::weekday_enum::Saturday);
+    }
+    boost::gregorian::date scheduler::last_sunday() const
+    {
+        return find_last_teenth_day(m_date, boost::gregorian::greg_weekday::weekday_enum::Sunday);
+    }
+}  // namespace meetup
 
 int main() {
-    using namespace robot_name;
-    robot r;
-    std::cout << r.name();
-    std::cout << '\n';
-    r.reset();
-    std::cout << r.name();
-    std::cout << '\n';
-    r.reset();
-    std::cout << r.name();
-    std::cout << '\n';
-    r.reset();
-    std::cout << r.name();
-    std::cout << '\n';
-
+    const meetup::scheduler meetup{ boost::gregorian::May, 2013 };
+    const boost::gregorian::date expected{ 2013, boost::gregorian::May, 13 };
+    cout << (expected == meetup.monteenth()) << endl;
     return 0;
 }
