c177ae236daa753f3cd943fe091e52f580ca20e7
Alexander Popov <lxndrppv@gmail.com>
2026-06-01 19:51:49 +0300

    feat: solution to series

--- ConsoleApp/main.cpp
+++ ConsoleApp/main.cpp
@@ -3,57 +3,30 @@
 #include <iostream>
 #include <stdexcept>
 #include <string>
+#include <vector>
 using std::cout;
 using std::endl;
 using namespace std::string_literals;
 
-namespace phone_number {
-    class phone_number {
-    public:
-        phone_number(const std::string& n);
-        std::string number() const;
-    private:
-        std::string clean_number(const std::string& n);
-        std::string m_number{};
-    };
-
-    phone_number::phone_number(const std::string& n) : m_number{ clean_number(n) } {}
-
-    std::string phone_number::number() const
-    {
-        return m_number;
-    }
-
-    namespace {
-        int char_to_int(char c) {
-            return c - '0';
+namespace series {
+    auto slice(const std::string& input, int n) {
+        std::vector<std::string> r{};
+        if (static_cast<size_t>(n) == input.size()) {
+            r.push_back(input);
+            return r;
         }
-    }
-
-    std::string phone_number::clean_number(const std::string& n)
-    {
-        std::string trimmed{ n };
-        if (trimmed.size() < 10) throw std::domain_error("Incorrect input number.");
-        trimmed.erase(remove_if(trimmed.begin(), trimmed.end(), isspace), trimmed.end());
-        for (size_t i{0}; i < trimmed.size(); ++i) {
-            char c = trimmed[i];
-            if (isdigit(c)) continue;
-            if (c == '+' || c == '.' || c == '-' || c == '(' || c == ')') continue;
-            throw std::domain_error("Incorrect input number.");
+        if (n < 1 || static_cast<size_t>(n) > input.size()) {
+            throw std::domain_error("Incorrect slice size.");
         }
-        trimmed.erase(remove_if(trimmed.begin(), trimmed.end(), ispunct), trimmed.end());
+        for (size_t i{0}, limit{input.size() - n}; i < limit; ++i) {
+            r.push_back(input.substr(i, n));
+        } 
 
-        if (trimmed[0] == '1' && trimmed.size() > 10) trimmed.erase(0, 1);
-        if (char_to_int(trimmed[0]) < 2) throw std::domain_error("Incorrect input number.");
-        if (char_to_int(trimmed[3]) < 2) throw std::domain_error("Incorrect input number.");
-
-        if (trimmed.size() > 10) throw std::domain_error("Incorrect input number.");
-        return trimmed;
+        return r;
     }
-}  // namespace phone_number
+}  // namespace series
 
 int main() {
-    //phone_number::phone_number("223.456.7890");
-    phone_number::phone_number("(223) 456-7890");
+    //series::slice("9142", 2)
     return 0;
 }
