d211950071a11cae6b5b548f5fda674b1e457b6e
Alexander Popov <lxndrppv@gmail.com>
2026-06-06 14:39:22 +0300

    feat: solution to word count

--- ConsoleApp/main.cpp
+++ ConsoleApp/main.cpp
@@ -3,59 +3,73 @@
 #include <format>
 #include <iostream>
 #include <string>
+#include <map>
+#include <format>
+#include <vector>
 using namespace std::string_literals;
 
-namespace bob {
-    static bool IsQuestion(const std::string& i) {
-        constexpr char kQuestion = '?';
-        return i.back() == kQuestion;
-    }
+namespace word_count {
+    std::string trimmedWord(const std::string& input) {
+        if (!std::any_of(input.begin(), input.end(), isalnum)) return ""s; 
+    
+        std::string word{ input };
+        word.erase(std::remove_if(word.begin(), word.end(), isspace), word.end());
+        if (word.size() == 0) return word;
 
-    static bool IsShouting(const std::string& i) {
-        bool hasLowercaseLetters{ false };
-        bool hasCapitalLetters{ false };
-        bool hasLetters{ false };
-        for (auto c : i) {
-            if (!isalpha(c)) continue;
-            hasLowercaseLetters = hasLowercaseLetters || islower(c);
-            hasCapitalLetters = hasCapitalLetters || isupper(c);
-        }
-        return hasCapitalLetters && !hasLowercaseLetters;
-    }
+        std::vector<size_t> indices{};
+        for (size_t i{ 0 }; i < word.size(); ++i) {
+            auto c = word[i];
 
-    static bool IsNothing(const std::string& i) {
-        return i.size() == 0;
-    }
-
-    std::string hey(const std::string& i)
-    {
-        std::string trimmed{ i };
-        trimmed.erase(std::remove_if(trimmed.begin(), trimmed.end(), isspace), trimmed.end());
-        if (IsNothing(trimmed)) {
-            return "Fine. Be that way!"s;
-        }
-        if (IsShouting(trimmed)) {
-            if (IsQuestion(trimmed)) {
-                return "Calm down, I know what I'm doing!"s;
+            if (ispunct(c)) {
+                if (i == 0 || i == word.size() - 1 || (!isalpha(word[i - 1]) || !isalpha(word[i + 1]))) {
+                    indices.push_back(i);
+                }                
             }
-            return "Whoa, chill out!"s;
         }
-        if (IsQuestion(trimmed)) {
-            return "Sure."s;
+
+        std::for_each(indices.rbegin(), indices.rend(), [&](int i) { word.erase(word.begin() + i); });
+
+        for (auto i{word.begin()}; i != word.end(); ++i) {
+            *i = std::tolower(*i);
         }
-        return "Whatever."s;
+
+        return word;
     }
-}  // namespace bob
+
+    std::map<std::string, int> words(const std::string& input) {
+        std::map<std::string, int> r{};
+        auto next = input.begin();
+        auto last = next;
+        while (next != input.end()) {
+            last = next;
+            next = std::find_if(next + 1, input.end(), [](const char c) { 
+                return !isalnum(c) && c != '\'';
+            }); 
+            if (last == next) {
+                break;
+            }
+
+            std::string word = trimmedWord({ last, next });
+            if (word.size() > 0) {
+                r[word]++;
+            }
+        }
+
+        return r;
+    }
+}  // namespace word_count
+
 
 int main() {
-    //std::cout << std::format("{}\n", bob::hey("FCECDFCAAB"));
-    //std::cout << std::format("{}\n", bob::hey("Tom - ay - to, tom - aaaah - to."));
-    std::cout << std::format("{}\n", bob::hey("Does this cryogenic chamber make me look fat?"));
-    std::cout << std::format("{}\n", bob::hey("WHAT THE HELL WERE YOU THINKING?"));
-    std::cout << std::format("{}\n", bob::hey("WHAT THE HELL WERE YOU THINKING? "));
-    std::cout << std::format("{}\n", bob::hey("1, 2, 3 GO!"));
-    std::cout << std::format("{}\n", bob::hey("1, 2, 3"));
-    std::cout << std::format("{}\n", bob::hey(""));
-    std::cout << std::format("{}\n", bob::hey("    "));
-    return 0;
+    //const std::map<std::string, int> expected{ {"word", 1} };
+    //const std::map<std::string, int> expected{ {"Joe can't tell between app, apple and a.", 1} };
+    //auto r = word_count::words("Joe can't tell between app, apple and a.");
+    //auto r = word_count::words("can't cant,can't");
+    auto r = word_count::words("'First: don't laugh. Then: don't cry. You're getting it.'");
+    //auto r = word_count::words("");
+    //auto r = word_count::words("one,two,three");
+    //auto r = word_count::words(",\n,one,\n ,two \n 'three'");
+    //auto r = word_count::words("can, can't, 'can't'");
+    //REQUIRE(expected == word_count::words("word"));
+     return 0;
 }
