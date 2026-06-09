e5a03155f84c72c55c8e6591ba7fe9fa4f072d5a
Alexander Popov <lxndrppv@gmail.com>
2026-06-09 14:05:26 +0300

    feat: solution to bracket match

--- ConsoleApp/main.cpp
+++ ConsoleApp/main.cpp
@@ -11,31 +11,56 @@
 #include <cmath>
 #include <iomanip>
 #include <ios>
+#include <stack>
 using namespace std::string_literals;
 
-namespace etl {
-    std::map<char, int> transform(const std::map<int, std::vector<char>>& other)
-    {
-        std::map<char, int> r{};
-
-        std::for_each(other.begin(), other.end(), [&](auto pair) {
-            
-            auto [points, characters] = pair;
-            std::for_each(characters.begin(), characters.end(), [&](const char c) {
-                r[std::tolower(c)] = points;
-            });
-        }); 
-
-        return r;
+namespace {
+    bool isBracket(char c) {
+        return c == '(' || c == ')' || c == '[' || c == ']' || c == '{' || c == '}';
     }
-}  // namespace etl
+
+    bool isClosing(char o, char c) {
+        return  (o == '(' && c == ')') || (o == '[' && c == ']') || (o == '{' && c == '}');
+    }
+}
+
+namespace matching_brackets {    
+    bool check(const std::string& input) {
+        // find brackets & put in stack
+        std::stack<char> brackets{};
+
+        for (auto i{ input.begin() }; i != input.end(); ++i) {
+            char current = *i;
+            if (!isBracket(current)) continue;
+            // if anything in stack, check for match
+            if (!brackets.empty()) {
+                auto top = brackets.top();
+                if (isClosing(top, current)) {
+                    brackets.pop();
+                    continue;
+                }
+            }
+            brackets.push(current);
+        }
+
+        return brackets.empty();
+    }
+}  // namespace matching_brackets
 
 int main() {
-    const std::map<int, std::vector<char>> old{ {1, {'A', 'E', 'I', 'O', 'U'}} };
-    const auto actual = etl::transform(old);
-    const std::map<char, int> expected{
-        {'a', 1}, {'e', 1}, {'i', 1}, {'o', 1}, {'u', 1} };
-    //REQUIRE(expected == actual);    
-    std::cout << std::boolalpha << (actual == expected) << '\n';
+    std::cout << std::boolalpha;
+    std::cout << matching_brackets::check("");
+    std::cout << matching_brackets::check("[]");
+    std::cout << !matching_brackets::check("}{");
+    std::cout << !matching_brackets::check("[[");
+    std::cout << !matching_brackets::check("{]");
+    std::cout << matching_brackets::check("{ }");
+    std::cout << !matching_brackets::check("{[])");
+    std::cout << matching_brackets::check("{}[]");
+    std::cout << matching_brackets::check("([{}({}[])])");
+    std::cout << !matching_brackets::check("{[)][]}");
+    std::cout << !matching_brackets::check("([{])");
+    std::cout << !matching_brackets::check("[({]})");
+    std::cout << !matching_brackets::check("[({}])");
     return 0;
 }
