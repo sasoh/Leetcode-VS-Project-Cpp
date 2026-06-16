4f9a73d53ea602ea3c30df233d11d5c364e2cfe6
Alexander Popov <lxndrppv@gmail.com>
2026-06-16 12:06:11 +0300

    feat: solution to acronym

--- ConsoleApp/main.cpp
+++ ConsoleApp/main.cpp
@@ -3,46 +3,43 @@
 #include <format>
 #include <string>
 
-namespace luhn {
-    bool valid(const std::string& input);
-    
-    bool valid(const std::string& input)
-    {
-        std::string temp{input};
-        temp.erase(std::remove_if(temp.begin(), temp.end(), isspace), temp.end());
-        if (input.size() < 2) return false;
-        
-        if (std::remove_if(temp.begin(), temp.end(), isalpha) != temp.end()
-            || std::remove_if(temp.begin(), temp.end(), ispunct) != temp.end()) return false;
+namespace acronym {
+    //acronym::acronym("Portable Network Graphics")
+    std::string acronym(const std::string& input) {
+        // Punctuation is handled as follows: hyphens are word separators(like whitespace); 
+        // all other punctuation can be removed from the input.
 
-        int sum{};
-        int size = static_cast<int>(temp.size());
-        for (int i{ size - 1 }; i >= 0; --i) {
-            const int digit = std::stoi(temp.substr(i, 1));
-            if ((size - i) % 2 == 0) {
-                int doubled = 2 * digit;
-                if (doubled > 9) {
-                    sum += doubled - 9;
-                }
-                else {
-                    sum += doubled;
-                }
-            }
-            else {
-                sum += digit;
+        std::string temp{ input };
+        for (size_t i{ 0 }, limit = temp.size(); i < limit; ++i) {
+            if (temp[i] == '-') {
+                temp[i] = ' ';
             }
         }
-        
+        temp.erase(std::remove_if(temp.begin(), temp.end(), ispunct), temp.end());
 
-        return (sum % 10 == 0);
+        // trimmed & ready for abbreviation
+        // first + every other after a space
+        std::string r{};
+
+        bool shouldIncludeNext{ false };
+        for (size_t i{ 0 }, limit = temp.size(); i < limit; ++i) {
+            auto &c = temp[i];
+            if (i == 0 || shouldIncludeNext) {
+                if (isspace(c)) continue;
+                shouldIncludeNext = false;
+                r.push_back(std::toupper(c));
+            }
+            if (isspace(c)) {
+                shouldIncludeNext = true;
+            }
+        }
+
+        return r;
     }
-}  // namespace luhn
+}  // namespace acronym
 
 int main() {
-    //luhn::valid("1")
-    //luhn::valid("059");
-    //luhn::valid("59");
-    luhn::valid("055 444 285");
-    //luhn::valid("8273 1232 7352 0569");
+    const std::string actual = acronym::acronym("Portable Network Graphics");
+
     return 0;
 }
