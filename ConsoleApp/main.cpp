5c0ef4a95c46029bc5c8ad7d85377044438a073d
Alexander Popov <lxndrppv@gmail.com>
2026-06-16 12:15:32 +0300

    feat: solution to armstrong number

--- ConsoleApp/main.cpp
+++ ConsoleApp/main.cpp
@@ -1,3 +1,23 @@
-﻿int main() {
+﻿#include <string>
+#include <cmath>
+
+namespace armstrong_numbers {
+    //armstrong_numbers::is_armstrong_number(5)
+    bool is_armstrong_number(int number) {
+        //An Armstrong number is a number that is the sum of its own digits each raised to the power of the number of digits.
+        size_t digitCount = std::to_string(number).size();
+        auto sum{ 0 };
+        int temp{ number };
+        do {
+            int rem = temp % 10;
+            temp /= 10;
+            sum += std::pow(rem, digitCount);
+        } while (temp > 0);
+
+        return (sum == number);
+    }
+}  // namespace armstrong_numbers
+
+int main() {
     return 0;
 }
